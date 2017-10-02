221aaa03e9743ffd486cb8c353d48b4e07aa4de2
Forbrig <vitorforbrig@gmail.com>
2017-10-02 00:23:00 -0300

    vigenere working if use only uppercase or lowercase

--- source/vigenere/main.c
+++ source/vigenere/main.c
@@ -11,19 +11,17 @@ int main ( ) {
 	fgets (msg, MAX_BUF, stdin);
 	//scanf("%s", msg);
 	printf("Write a key\n");
-	fgets (key, MAX_BUF, stdin);
+	fgets(key, MAX_BUF, stdin);
 	
 	repeated_key = vc_repeat_key(msg, key);
 	printf("Repeated key: %s\n", repeated_key);
 	
-	/*
-	encrypt = vc_encryption(msg, key);
+	encrypt = vc_encryption(msg, repeated_key);
 	printf("Encrypted: %s\n", encrypt);
 
-	decrypt = vc_decryption(encrypt, key);
+	decrypt = vc_decryption(encrypt, repeated_key);
 	printf("Decrypted: %s\n", decrypt);
-	*/
 	
-	//system("pause");
+	system("pause");
 	return 0;
 }
--- source/vigenere/vigenere_func.c
+++ source/vigenere/vigenere_func.c
@@ -4,17 +4,14 @@ char *vc_repeat_key(char* msg, char* key) {
 	char* repeated_key = malloc(MAX_BUF * sizeof(char));
 	int i;
 	int j = 0;
-	
+	int k = 0;
 	for (i = 0; i < strlen(msg); i++) {
-		
-		repeated_key[i] = key[i % (strlen(key)-1)];
-		
-		
-		/*
-		if (i > strlen(key - 1)) {
-			printf("%d\n", i % (strlen(key)-1));
+		if (msg[i] == ' ') {
+			repeated_key[i] = ' ';
+			k++;	
+		} else {
+			repeated_key[i] = key[(i - k) % (strlen(key)-1)];	
 		}
-		*/
 		j++;
 	}
 	repeated_key[j-1] = '\0';
@@ -24,7 +21,25 @@ char *vc_repeat_key(char* msg, char* key) {
 char *vc_encryption(char* msg, char* key) {
 	char* encrypt = malloc(MAX_BUF * sizeof(char));
 	strcpy(encrypt, msg);
-	
+	int i;
+	int aux;
+	for (i = 0; i < strlen(msg); i++) {
+		if (msg[i] >= 'a' && msg[i] <= 'z') {
+			if (key[i] + (msg[i] - 'a') > 'z') {
+				aux = key[i] + (msg[i] - 'a') - 'z';
+				encrypt[i] = 'a' + aux - 1;
+			} else {
+				encrypt[i] =  key[i] + (msg[i] - 'a');
+			}
+		} else if (msg[i] >= 'A' && msg[i] <= 'Z') {
+			if (key[i] + (msg[i] - 'A') > 'Z') {
+				aux = key[i] + (msg[i] - 'A') - 'Z';
+				encrypt[i] = 'A' + aux - 1;
+			} else {
+				encrypt[i] =  key[i] + (msg[i] - 'A');
+			}
+		} 
+	}
 	return encrypt;
 }
 
@@ -32,6 +47,26 @@ char *vc_decryption(char* msg, char* key) {
 	char* decrypt = malloc(MAX_BUF * sizeof(char));
 	strcpy(decrypt, msg);
 	
+	int i;
+	int aux;
+	for (i = 0; i < strlen(msg); i++) {
+
+		if (msg[i] >= 'a' && msg[i] <= 'z') {
+			if (msg[i] - (key[i] - 'a') < 'a' ) {
+				aux = 'a' - (msg[i] - (key[i] - 'a'));
+				decrypt[i] = 'z' - aux + 1;
+			} else {
+				decrypt[i] = msg[i] - (key[i] - 'a');
+			}
+		} else if (msg[i] >= 'A' && msg[i] <= 'Z') {
+			if (msg[i] - (key[i] - 'A') < 'A' ) {
+				aux = 'A' - (msg[i] - (key[i] - 'A'));
+				decrypt[i] = 'Z' - aux + 1;
+			} else {
+				decrypt[i] = msg[i] - (key[i] - 'A');
+			}
+		}
+	}
 
 	return decrypt;
 }
