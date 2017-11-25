73fb194c22c01e6a1e677bd11084c3218610e79f
marcoputon <marcoputon@gmail.com>
2017-11-25 21:41:03 -0200

    Prints in vigenere files

--- source/vigenere/main.c
+++ source/vigenere/main.c
@@ -14,13 +14,12 @@ int main ( ) {
 	if (in == NULL) {
 		printf("Cannot open the file...\n");
 		exit(0);
-	} else {		
-		printf("Message to encrypt\n");
-		msg = file_to_string(in);
-		printf("%s\n", msg);
-	
-		printf("Write a key\n");
+	} else {
+		printf("Write a key: \n");
 		fgets(key, MAX_BUF, stdin);
+
+		msg = file_to_string(in);
+		printf("\nMESSAGE TO ENCRYPT:\n%s\n", msg);
 
 		//ignore the /n from the end of the string
 		char auxkey[strlen(key)-1];
@@ -33,20 +32,20 @@ int main ( ) {
 		*/
 
 		repeated_key = vc_repeat_key(msg, auxkey);
-		printf("Repeated key:\n%s\n", repeated_key);
-	
-		encrypt = vc_encryption(msg, repeated_key);
-		printf("Encrypted:\n%s\n", encrypt);
+		printf("\nREPEATED KEY:\n%s\n", repeated_key);
 
+		encrypt = vc_encryption(msg, repeated_key);
+		printf("\nENCRYPTED MESSAGE:\n%s\n", encrypt);
 
 		fclose(fopen("test/out1.txt", "w")); //clear the content
 		FILE *out = fopen("test/out1.txt", "ab+");
 		string_to_file(encrypt, out); // trows the encrypted content to a file
-	
-		
+
+
 		decrypt = vc_decryption(encrypt, repeated_key);
-		printf("Decrypted:\n%s\n", decrypt);
-		
+		printf("\nDECRYPTED MESSAGE:\n%s\n", decrypt);
+
+
 		/*
 		fclose(in);
 		fclose(out);
